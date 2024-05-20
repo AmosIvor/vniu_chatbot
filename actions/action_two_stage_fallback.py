f60fe6e7eb3b30df0a328c7867aab478bafe0cdd
AmosIvor <trantuanvu00000@gmail.com>
2024-05-20 11:58:40 +0700

    [TWO STAGE FALLBACK] config chatbot for two stage fallback

---
+++ actions/action_two_stage_fallback.py
@@ -0,0 +1,18 @@
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import UserUtteranceReverted
+
+class ActionTwoStageFallback(Action):
+
+    def name(self) -> Text:
+        return "action_two_stage_fallback"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(response="utter_ask_rephrase")
+
+        return [UserUtteranceReverted()]
